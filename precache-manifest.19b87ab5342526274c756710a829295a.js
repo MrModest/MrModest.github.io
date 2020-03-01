184a4cc05469420f7b3deadc01a6055d6ecd7b8e
MrModest <babaevkamil@gmail.com>
2020-03-01 13:48:49 +0300

    Updates

---
+++ precache-manifest.19b87ab5342526274c756710a829295a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8182f0e4b78fda1fb28e1302ddb55704",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "3317f48d810977bd5672",
+    "url": "/contacts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cfaa72fffb49ea64d667",
+    "url": "/contacts/static/js/2.56523127.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/contacts/static/js/2.56523127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3317f48d810977bd5672",
+    "url": "/contacts/static/js/main.1395dd7d.chunk.js"
+  },
+  {
+    "revision": "143f8392b4eb7b811270",
+    "url": "/contacts/static/js/runtime-main.3b76352d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contacts/static/media/logo.5d5d9eef.svg"
+  }
+]);
