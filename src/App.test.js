a1ed75c6b152be3b7922741859ae1a18585bc26e
Kamil <babaevkamil@gmail.com>
2021-03-16 12:01:14 +0300

    Delete App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AppWrapper from './AppWrapper';
-
-test('renders For communicate header', () => {
-  const { getByText } = render(<AppWrapper />);
-  const linkElement = getByText(/For communicate/i);
-  expect(linkElement).toBeInTheDocument();
-});
