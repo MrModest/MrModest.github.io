04f294a7c83613a7236e37336d9766e41a293564
MrModest <babaevkamil@gmail.com>
2021-03-18 23:21:56 +0300

    draft for CardEditPopup

---
+++ src/components/CardEditPopup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as api from '../api';
+
+import Card from '../entities/Card';
+
+import styles from './CardEditPopup.module.css';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+const CardEditPopup = ({ label, logo, fullname, responseTime, link, description }: Card) => {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default CardEditPopup;
